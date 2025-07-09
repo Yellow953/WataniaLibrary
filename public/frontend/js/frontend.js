b8fa7e48c8b5d7ca2eefea7637d7cc8107c3be91
Yellow <joemazloum953@gmail.com>
2025-07-09 20:14:55 +0300

    home page optimization

--- public/frontend/js/frontend.js
+++ public/frontend/js/frontend.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $(".products")({
-        items: 4,
-        loop: false,
-        margin: 20,
+    $(".product-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
         nav: false,
-        dots: true,
+        dots: false,
         responsive: {
             0: { items: 2 },
-            600: { items: 3 },
-            1080: { items: 4 },
+            768: { items: 3 },
+            992: { items: 4 },
+            1200: { items: 5 },
         },
     });
 });
